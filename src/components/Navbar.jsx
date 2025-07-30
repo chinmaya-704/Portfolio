fe1242b8d1595ee981ff96acd2ad989a4dc33be4
Chinmaya <b122044@iiit-bh.ac.in>
2025-07-31 00:52:02 +0530

    text size change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
    return (
      <nav className="flex justify-between items-center py-6 px-10">
 
-       <h1 className="text-2xl font-bold text-white pacifico">Chinmaya<span className="text-indigo-600 hover:underline">...</span></h1>
+       <h1 className="text-2xl font-bold text-white pacifico max-md:text-xs">Chinmaya<span className="text-indigo-600 hover:underline">...</span></h1>
 
-       <ul className="flex space-x-6 text-white text-sm montser">
+       <ul className="flex max-md:space-x-2 space-x-6 text-white text-sm montser max-md:text-xs">
 
          <li><Link to="/" className="hover:text-indigo-600 hover:underline hover:font-bold visited:text-indigo-400">Home</Link></li>
          <li><Link to="/about" className="hover:text-indigo-600 hover:underline hover:font-bold visited:text-indigo-400">About</Link></li>
