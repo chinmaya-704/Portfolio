5a6e82397556e7883faff1b5dd61fc0f8b9f1fd3
Chinmaya <b122044@iiit-bh.ac.in>
2025-07-31 17:25:18 +0530

    logo-cursor-pointer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,14 @@ const Navbar = () => {
    return (
      <nav className="flex justify-between items-center py-6 px-7 max-md:px-3">
 
-       <h1 className="text-2xl font-bold text-white pacifico max-md:text-xs">Chinmaya<span className="text-indigo-600 hover:underline">...</span></h1>
+       <h1 className="text-2xl font-bold text-white pacifico max-md:text-xs cursor-pointer">Chinmaya<span className="text-indigo-600">...</span></h1>
 
        <ul className="flex max-md:space-x-2 space-x-6 text-white text-sm montser max-md:text-xs">
 
-         <li><Link to="/" className="hover:text-indigo-600 hover:underline hover:font-bold visited:text-indigo-400">Home</Link></li>
-         <li><Link to="/about" className="hover:text-indigo-600 hover:underline hover:font-bold visited:text-indigo-400">About</Link></li>
-         <li><Link to="/projects" className="hover:text-indigo-600 hover:underline hover:font-bold visited:text-indigo-400">Projects</Link></li>
-         <li><Link to="/contact" className="hover:text-indigo-600 hover:underline hover:font-bold visited:text-indigo-400">Contact</Link></li>
+         <li><Link to="/" className="hover:text-indigo-600 hover:font-bold ">Home</Link></li>
+         <li><Link to="/about" className="hover:text-indigo-600 hover:font-bold ">About</Link></li>
+         <li><Link to="/projects" className="hover:text-indigo-600 hover:font-bold ">Projects</Link></li>
+         <li><Link to="/contact" className="hover:text-indigo-600 hover:font-bold ">Contact</Link></li>
 
 
        </ul>
