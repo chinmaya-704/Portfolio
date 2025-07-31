744e2e37b970a2fadee73f064e8340ba081d9eeb
Chinmaya <b122044@iiit-bh.ac.in>
2025-07-31 20:48:29 +0530

    minor change in files

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base: '/',
   plugins: [
     tailwindcss(),react()
   ],
-  // build:{
-  //   outDir: 'dist',
-  //   // rollupOptions: {
-  //   //   input: {
-  //   //     main: './index.html'
-  //   //   }
-  //   // }
-  // }
+  build:{
+    outDir: 'dist',  //   // rollupOptio
+  }
 })
