1c91859a74835e39731283bcacd2019d8afa8664
Chinmaya <b122044@iiit-bh.ac.in>
2025-07-31 20:23:31 +0530

    config file changed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    tailwindcss(),react()
   ],
+  build:{
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
 })
